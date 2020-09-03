f4b4da07eadb3456e62abf3f4cd313885ddf8444
leonardo.bottaro <leonardooloiro@hotmail.com>
2020-09-03 20:40:13 -0300

    #new 9 - resource Get /clientes

---
+++ src/main/java/com/example/aula_lab_noite_27_08_2020/controller/Cliente.java
@@ -0,0 +1,8 @@
+package com.example.aula_lab_noite_27_08_2020.controller;
+
+public class Cliente {
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public double saldo;
+}
--- src/main/java/com/example/aula_lab_noite_27_08_2020/controller/ClienteController.java
+++ src/main/java/com/example/aula_lab_noite_27_08_2020/controller/ClienteController.java
@@ -1,22 +1,71 @@
 package com.example.aula_lab_noite_27_08_2020.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ClienteController {
-    
+
+    List <Cliente> clientes;
+
+    @PostConstruct
+    public void criarClientes(){
+        final Cliente c1 = new Cliente();
+        final Cliente c2 = new Cliente();
+        final Cliente c3 = new Cliente();
+
+        c1.codigo = 1;
+        c1.nome = "Jose";
+        c1.endereco = "Rua X,99";
+        c1.saldo = 100;
+
+        c2.codigo = 1;
+        c2.nome = "Maria";
+        c2.endereco = "Rua Y,123";
+        c2.saldo = 200;
+
+        c3.codigo = 1;
+        c3.nome = "Fernanda";
+        c3.endereco = "Rua Q,25";
+        c3.saldo = 400;
+
+        clientes = Arrays.asList(c1, c2, c3);
+
+    }
+
     @GetMapping("/clientes")
-    public String getClientes(){
-        return "Vai algum dia retornar do BD TODOS os clientes!";
+    public List<Cliente> getClientes() {
+        return clientes;
     }
 
     @GetMapping("/clientes/{codigo}")
-    public String getCliente1(@PathVariable int codigo){
-        if (codigo > 0)
-            return "Vai algum dia retornar um cliente" + codigo;
-        else
-            return "Erro, codigo negativo" + codigo;
+    public Cliente getClientes(@PathVariable int codigo) {
+        Cliente cli=null;
+
+        /*
+        for(int i=0; i < clientes.size();i++){
+            Cliente aux = clientes.get(i);
+            if(aux.codigo == codigo){
+                cli = aux;
+                break;
+            }
+        }
+        */
+
+        for(Cliente aux: clientes){
+            if(aux.codigo == codigo){
+                cli = aux;
+                break;
+            }
+        }
+
+        return cli;
     }
 }
